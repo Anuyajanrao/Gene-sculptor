3fb875abff4e9489460f711a85eb22ad74937ab0
Anuyajanrao <3522411013@despu.edu.in>
2025-05-13 15:23:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import plotly.io as pio
 import plotly.graph_objects as go
 from Bio import SeqIO
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
